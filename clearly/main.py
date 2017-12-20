94dd506888456ae48cb34eddf0b15b88bdbe6546
hy_ice <hyice.zhang@gmail.com>
2017-12-20 16:29:09 +0800

    修改gitignore 忽略IDE相关文件

---
+++ clearly/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import wx
+
+
+def main():
+    app = wx.App()
+    wx.Frame().Show()
+    app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
