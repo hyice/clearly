99460d24ffd1401599d63a3167ce987e0a695df1
hy_ice <hyice.zhang@gmail.com>
2017-12-22 16:02:38 +0800

    完成图片的展示和缩放

--- clearly/main.py
+++ clearly/main.py
@@ -2,11 +2,27 @@
 # -*- coding: utf-8 -*-
 
 import wx
+import preview
+import os
+
+
+def show_file_chooser(parent=None, title=u'选择文件', wildcard="*"):
+    dialog = wx.FileDialog(parent, title, os.getcwd(), style=wx.FD_OPEN, wildcard=wildcard)
+    result = dialog.ShowModal()
+    file_path = None
+    if result == wx.ID_OK:
+        file_path = dialog.GetPath()
+    dialog.Destroy()
+    return file_path
 
 
 def main():
     app = wx.App()
-    wx.Frame().Show()
+
+    path = show_file_chooser()
+    if path:
+        preview.PreviewFrame(path).Show()
+
     app.MainLoop()
 
 
