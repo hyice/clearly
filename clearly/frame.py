c00e85de0dfc1910563984c67a7226041e7f8b6a
hy_ice <hyice.zhang@gmail.com>
2017-12-25 11:12:29 +0800

    绑定ESC键用于退出程序

--- clearly/frame.py
+++ clearly/frame.py
@@ -8,4 +8,22 @@ import view
 class PreviewFrame(wx.Frame):
     def __init__(self, file_path):
         wx.Frame.__init__(self, None, -1, size=(500, 500))
-        view.PreviewPanel(self, file_path)
+        self.view = view.PreviewPanel(self, file_path)
+
+        self._bind_events()
+
+    def Show(self, show=True):
+        self.ShowFullScreen(show)
+
+    # ----------------------------------------
+    # 事件绑定
+
+    def _bind_events(self):
+        self.view.Bind(wx.EVT_KEY_DOWN, self._handle_key_press)
+
+    def _handle_key_press(self, event):
+        key_code = event.GetKeyCode()
+        if key_code == wx.WXK_ESCAPE:  # ESC 键退出程序
+            self.Close()
+
+        event.Skip()
